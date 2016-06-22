d63eea50a70250f2c48d8bc28f4155b4eebbf947
Ramanathan.M <rus.cahimb@gmail.com>
2016-06-22 11:04:12 +0530

    show usage of zip with examples

--- whats_izip.py
+++ whats_izip.py
@@ -15,8 +15,17 @@ nations = ['Brazil', 'Russia', 'India', 'China']
 capitals = ['Brasilia', 'Moscow', 'New Delhi', 'Beijing']
 
 ### zipper . . .
+print "==" * 10,
+print "print and zip using a for loop"
 for country, center in zip(nations, capitals):
     print "Capital of %s is %s" %(country, center)
+print "==" * 10
+
+# repeat of the above for loop in a single print
+print "==" * 10,
+print "print and zip in a single line of python code"
+print(list(zip(*(nations, capitals))))
+print "==" * 10
     
 ## izipper . . . 
 for this, that in izip(nations, capitals):
