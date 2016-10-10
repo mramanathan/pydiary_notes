d05bcc30a0c9f93b726e396a0f74021fd5df65ae
Ramanathan M <rus.cahimb@gmail.com>
2016-10-10 23:24:20 +0530

    Session, 239 details of MPs nominated to Rajya Sabha

--- opendata_india/rajyasabha_attendance.py
+++ opendata_india/rajyasabha_attendance.py
@@ -9,6 +9,37 @@ signed the member's attendance register during the 239 Session of Rajya Sabha fo
 period from 25-04-16 to 13-05-16.
 """
 
+def nominated_members(attend):
+
+    # 2: on read csv, get members filtered on states and use method on dataseries to get the names of nominated members
+    print("~@" * 3 + " List of nominated members " + "~@" *3)
+    member_list = attend.ix[:, 'State']
+    for member, type in member_list.iteritems():
+        if type == "Nominated":
+            print(member + " was " + type.lower() + " to the Rajya Sabha.")
+
+    return None
+
+
+def nominated_members_details(attend):
+
+    state_list = attend['State'] # mix of actual state names and nominations
+
+    nom = state_list == "Nominated" # 
+    print("~@" * 3 + " full list of nominations : " + "~@" *3)
+    
+    # output is garbled across multiple rows, but, atleast it works
+    print(attend[nom])
+    print("\n")
+
+    # Interested to know if they signed in the register
+    print("~@" * 3 + " And, did they remember to sign the register ? " + "~@" *3)
+    print(attend[nom].ix[:, 'No of days member signed the register'])
+
+    return None
+
+
+
 def main():
 
     ''' read csv and run the desired function as per user's input '''
@@ -17,25 +48,10 @@ def main():
     file = 'Rajyasabha_members_attendance_session239.csv'
     attend = pd.read_csv(file, index_col=2) # start the index with the MPs of Rajya Sabha
 
-    state_list = attend['State'] # mix of actual state names and nominations
-
-    nom = state_list == "Nominated" # 
-    print("~@" * 3 + " full list from nominations check : " + "~@" *3)
-    
-    # output is garbled across multiple rows, but, atleast it works
-    print(attend[nom])
-
-    # Interested to know if they signed in the register
-    print("~@" * 3 + " Did they remember to sign the register ? " + "~@" *3)
-    print(attend[nom].ix[:, 'No of days member signed the register'])
-
-
-    # 2: on read csv, get members filtered on states and use method on dataseries to get the names of nominated members
-    print("~@" * 3 + " List of nominated members " + "~@" *3)
-    member_list = attend.ix[:, 'State']
-    for member, type in member_list.iteritems():
-        if type == "Nominated":
-            print(member + " was " + type.lower() + " to the Rajya Sabha.")
+    nominated_members(attend)
+    print("\n")
+    nominated_members_details(attend)
+    print("\n")
 
 
 if __name__ == "__main__":
